1d1ddc1bca6340529601434b384a67e40bacaf8d
g3raphacs <g3raphacs@gmail.com>
2021-03-31 11:40:00 +0200

    add rooter

--- public/index.php
+++ public/index.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World !</h1>
-</body>
-</html>
+<?php
+require_once '../vendor/autoload.php';
+
+// use App\Twig;
+
+// Constants_____________________________________
+define("BASE_PATH","/MVC-base/") ;
+
+//ROUTER
+$uri = $_SERVER['REQUEST_URI'];
+$router = new AltoRouter();
+$router->setBasePath('');
+
+//Home Page __________________________________
+$router->map('GET', BASE_PATH, function () {
+    echo "Page d'Accueil";
+
+    //render template
+    // $twig = new Twig('base.html.twig');
+    // $twig->render([
+    //         'categories' => $categories->data,
+    //         // 'SERVER_URI' => SERVER_URI,
+    //     ]);
+});
+
+//404 Page __________________________________
+$router->map('GET', '/[*]', function () {
+    echo "Cette page n'existe pas";
+});
+
+
+
+// Launch Routing
+$match = $router->match();
+
+if( is_array($match) && is_callable( $match['target'] ) ) {
+    call_user_func_array( $match['target'], $match['params'] );
+} else {
+    // no route was matched
+    header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
